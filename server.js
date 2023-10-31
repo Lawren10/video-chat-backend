c31cd3dd0940efb0307031d4ab59033a636533e1
Lawren10 <lawrenceanthony70@gmail.com>
2023-10-31 07:44:00 +0100

    backend server conected initiated

--- server.js
+++ server.js
@@ -1,19 +1,76 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const http = require("http");
+const Server = require("socket.io").Server;
+const cors = require("cors");
+const calleWrtc = require("wrtc");
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const server = http.createServer(app);
 
-io.on("connection", async (socket) => {
-  console.log("socket connected");
+app.use(cors);
+const io = new Server(server, {
+ cors: {
+  origin: "*",
+ },
 });
 
-app.get("/", async (req, res) => {
-  res.send("App connected succesfully");
+io.on("connect", async (socket) => {
+ let socketId = socket.id;
+ console.log("socket connected", socketId);
+
+ // creating room
+ socket.on("createRoom", async (credential) => {
+  const server = {
+   "ice server": [
+    { url: "stun1.l.google.com:19302" },
+    { url: "stun2.l.google.com:19302" },
+    {
+     url: "turn:192.158.29.39:3478?transport=tcp",
+     credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+     username: "28224511:1379330808",
+    },
+   ],
+  };
+
+  const serverPeer = new calleWrtc.RTCPeerConnection(server);
+  serverPeer.setRemoteDescription(
+   new calleWrtc.RTCSessionDescription(credential.clientOffer)
+  );
+
+  // server
+
+  console.log("server remote disc set");
+
+  const serverAnswer = await serverPeer.createAnswer();
+  await serverPeer.setLocalDescription(serverAnswer);
+
+  console.log("server answer created and set as local discription");
+
+  socket.emit("getServerAnswer", serverAnswer);
+
+  socket.on("sendingIceCandidates", (clientIce) => {
+   console.log("client ice candidate", clientIce.iceCanditate);
+
+   serverPeer.addIceCandidate(clientIce.iceCanditate);
+  });
+
+  // const offer = await localPeer.createOffer();
+  console.log(serverPeer._listeners);
+
+  socket.serverWrtc = serverPeer;
+
+  socket.join(credential.roomId);
+  console.log(
+   "socket in room",
+   // io.sockets.adapter.rooms.get(credential.roomId),
+   socket.serverWrtc.localDescription,
+   socket.serverWrtc.remoteDescription,
+   socket.serverWrtc.iceGatheringState,
+   socket.serverWrtc.iceConnectionState
+  );
+ });
 });
 
 server.listen(8000, () => {
-  console.log("server running at http://localhost:8000");
+ console.log("server running at http://localhost:8000");
 });
